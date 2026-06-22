b017658b01b01730794e219ebb56a22ce9aff904
aliyum13 <158463984+aliyum13@users.noreply.github.com>
2026-06-22 10:23:25 +0100

    update: bundle prices - 3-pack ₦35,000, 5-pack ₦50,000

--- apps/core/constants.py
+++ apps/core/constants.py
@@ -2,8 +2,8 @@
 REPORT_PRICE_NGN = 15_000
 
 BUNDLE_PRICES_NGN = {
-    "3": 40_000,   # 3-report bundle
-    "5": 65_000,   # 5-report bundle
+    "3": 35_000,   # 3-report bundle
+    "5": 50_000,   # 5-report bundle
 }
 
 REPORT_TYPE = "us_vehicle"
